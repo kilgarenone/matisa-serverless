bba69f545c4f1fdfe58126aee8d76d7e35b8f312
Kheoh Yee Wei <kwei88@gmail.com>
2018-10-01 11:38:46 +0800

    Refactor 'getAccessToken' into its own file

---
+++ api/getAccessToken.js
@@ -0,0 +1,38 @@
+import got from "got";
+import { success, failure } from "../libs/response-lib";
+
+const OAUTH_URL =
+  "https://sandbox.hydrogenplatform.com/authorization/v1/oauth/token?grant_type=client_credentials";
+
+export async function getAccessToken(event, context, callback) {
+  const { SANDBOX_ATOM_CLIENT_ID, SANDBOX_ATOM_CLIENT_SECRET } = process.env;
+
+  const bufferCredential = Buffer.from(
+    `${SANDBOX_ATOM_CLIENT_ID}:${SANDBOX_ATOM_CLIENT_SECRET}`
+  );
+  const base64Credential = bufferCredential.toString("base64");
+
+  const requestConfig = {
+    method: "POST",
+    json: true,
+    headers: { Authorization: `Basic ${base64Credential}` }
+  };
+
+  try {
+    const res = await got(OAUTH_URL, requestConfig);
+
+    const response = success(
+      res.statusCode,
+      { loggedIn: true },
+      { "Set-Cookie": `access_token=${res.body.access_token}; HttpOnly; ` }
+    );
+
+    return callback(null, response);
+  } catch (error) {
+    const response = failure(error.statusCode, {
+      message: error.statusMessage
+    });
+
+    return callback(null, response);
+  }
+}
