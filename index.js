b33786dd226d381498fec6f5e6785ef04fa2d5e5
Kheoh Yee Wei <kwei88@gmail.com>
2018-10-01 11:38:08 +0800

    Extract 'getAccessToken' function

--- index.js
+++ index.js
@@ -1,89 +1,47 @@
-import serverless from 'serverless-http';
-import express from 'express';
-import querystring from 'querystring';
-import got from 'got';
+import serverless from "serverless-http";
+import express from "express";
+import querystring from "querystring";
+import got from "got";
 import {
   getAccessTokenFromCookie,
   getAllocationId,
   getRiskToleranceLevelType,
   sortArrayByDesc,
-  roundValuesUpToTarget,
-} from './utilities.js';
+  roundValuesUpToTarget
+} from "./utilities.js";
 
 const app = express();
-const OAUTH_URL =
-  'https://sandbox.hydrogenplatform.com/authorization/v1/oauth/token?grant_type=client_credentials';
-const BASE_URL = 'https://sandbox.hydrogenplatform.com/nucleus/v1';
+const BASE_URL = "https://sandbox.hydrogenplatform.com/nucleus/v1";
 
 const httpClient = accessToken =>
   got.extend({
     baseUrl: BASE_URL,
     json: true,
     headers: {
-      Authorization: `Bearer ${accessToken}`,
-    },
+      Authorization: `Bearer ${accessToken}`
+    }
   });
 
-let ACCESS_TOKEN = '';
+let ACCESS_TOKEN = "";
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
+app.get("/", (req, res) => {
+  res.send("Hello World");
 });
 
 export const handler = serverless(app);
-
-export async function getAccessToken(event, context) {
-  const { SANDBOX_ATOM_CLIENT_ID, SANDBOX_ATOM_CLIENT_SECRET } = process.env;
-
-  // Don't prettier format this line! It would cry SyntaxError invalid token ')' of sort. Weird i knw.
-  const bufferCredential = Buffer.from(
-    `${SANDBOX_ATOM_CLIENT_ID}:${SANDBOX_ATOM_CLIENT_SECRET}`,
-  );
-  const base64Credential = bufferCredential.toString('base64');
-
-  const requestConfig = {
-    method: 'POST',
-    json: true,
-    headers: { Authorization: `Basic ${base64Credential}` },
-  };
-
-  try {
-    const res = await got(OAUTH_URL, requestConfig);
-    return {
-      statusCode: res.statusCode,
-      headers: {
-        'Access-Control-Allow-Origin': '*', // Required for CORS support to work
-        'Access-Control-Allow-Credentials': true, // Required for cookies, authorization headers with HTTPS
-        'Set-Cookie': `access_token=${res.body.access_token}; HttpOnly; `, // TODO: add 'Secure'
-      },
-      body: JSON.stringify({ loggedIn: true }),
-    };
-  } catch (error) {
-    return {
-      statusCode: error.statusCode,
-      headers: {
-        'Access-Control-Allow-Origin': '*', // Required for CORS support to work
-        'Access-Control-Allow-Credentials': true, // Required for cookies, authorization headers with HTTPS
-      },
-      body: JSON.stringify({
-        message: error.statusMessage,
-      }),
-    };
-  }
-}
 
 // function(modelId: number[] | number) => Promise
 function fetchModelHolding(modelId) {
   const modelHoldingRequestConfig = {
     query: {
       filter: `model_id==${
-        Array.isArray(modelId) ? modelId.join(',model_id==') : modelId
-      }`,
-    },
+        Array.isArray(modelId) ? modelId.join(",model_id==") : modelId
+      }`
+    }
   };
   return httpClient(ACCESS_TOKEN).get(
-    '/model_holding',
-    modelHoldingRequestConfig,
+    "/model_holding",
+    modelHoldingRequestConfig
   );
 }
 
@@ -94,39 +52,71 @@ function fetchAllocation(allocationId) {
 function fetchAllocationComposition(allocationId) {
   const allocCompoRequestConfig = {
     query: {
-      filter: `allocation_id==${allocationId}`,
-    },
+      filter: `allocation_id==${allocationId}`
+    }
   };
   return httpClient(ACCESS_TOKEN).get(
-    '/allocation_composition',
-    allocCompoRequestConfig,
+    "/allocation_composition",
+    allocCompoRequestConfig
   );
 }
 
+const ACC_TYPE_TAX_ADVANTAGED = "101";
+const ACC_TYPE_TAXABLE = "100";
+
+const ACCOUNT_TYPE = {
+  [ACC_TYPE_TAX_ADVANTAGED]: {
+    name: "Tax-advantaged Investment Account",
+    id: "dad8a90e-76ed-42ad-8ca3-4e4048f20af7"
+  },
+  [ACC_TYPE_TAXABLE]: {
+    name: "Taxable Investment Account",
+    id: "12dde151-73aa-4f45-b2fa-73a618c36c92"
+  }
+};
+
+function createAccount(clientId, accountType) {
+  return httpClient(ACCESS_TOKEN).post("/account", {
+    body: {
+      name: ACCOUNT_TYPE[accountType].name,
+      account_type_id: ACCOUNT_TYPE[accountType].id,
+      clients: [
+        { client_id: clientId, client_account_association_type: "owner" }
+      ]
+    }
+  });
+}
 export async function registerClient(event, context, callback) {
   ACCESS_TOKEN = ACCESS_TOKEN || getAccessTokenFromCookie(event.headers.Cookie);
 
   const clientData = querystring.parse(event.body);
   console.log(clientData);
 
-  clientData['client_type'] = 'individual';
-  clientData['email'] = clientData.username;
-  clientData['metadata'] = JSON.parse(clientData.metadata);
+  clientData.client_type = "individual";
+  clientData.email = clientData.username;
+  clientData.metadata = JSON.parse(clientData.metadata);
 
   try {
-    const res = await httpClient(ACCESS_TOKEN).post('/client', {
-      body: clientData,
+    const clientRes = await httpClient(ACCESS_TOKEN).post("/client", {
+      body: clientData
     });
+
+    const clientId = clientRes.body.client.id;
+
+    const [taxAdvantagedAcc, taxableAcc] = await Promise.all([
+      createAccount(clientId, ACC_TYPE_TAX_ADVANTAGED),
+      createAccount(clientId, ACC_TYPE_TAXABLE)
+    ]);
 
     const response = {
       statusCode: 200,
       headers: {
-        'Access-Control-Allow-Origin': 'http://localhost:3000', // Need to properly set origin to receive response!
-        'Access-Control-Allow-Credentials': true, // Required for cookies, authorization headers with HTTPS
+        "Access-Control-Allow-Origin": "http://localhost:3000", // Need to properly set origin to receive response!
+        "Access-Control-Allow-Credentials": true // Required for cookies, authorization headers with HTTPS
       },
       body: JSON.stringify({
-        client: res.body,
-      }),
+        client: res.body
+      })
     };
 
     callback(null, response);
@@ -134,12 +124,12 @@ export async function registerClient(event, context, callback) {
     const response = {
       statusCode: error.statusCode,
       headers: {
-        'Access-Control-Allow-Origin': '*', // Required for CORS support to work
-        'Access-Control-Allow-Credentials': true, // Required for cookies, authorization headers with HTTPS
+        "Access-Control-Allow-Origin": "*", // Required for CORS support to work
+        "Access-Control-Allow-Credentials": true // Required for cookies, authorization headers with HTTPS
       },
       body: JSON.stringify({
-        message: error.statusMessage,
-      }),
+        message: error.statusMessage
+      })
     };
 
     callback(null, response);
@@ -152,13 +142,13 @@ export async function getRecommendedPortfolio(event, context, callback) {
   const surveyResults = querystring.parse(event.body);
   const totalRiskScore = Object.values(surveyResults).reduce(
     (accumulator, currentVal) => +accumulator + +currentVal,
-    0,
+    0
   );
 
   const riskToleranceLevelType = getRiskToleranceLevelType(totalRiskScore);
   const allocationId = getAllocationId(
     surveyResults.age,
-    riskToleranceLevelType,
+    riskToleranceLevelType
   );
 
   try {
@@ -171,13 +161,13 @@ export async function getRecommendedPortfolio(event, context, callback) {
         acc[model_id] = { weight };
         return acc;
       },
-      {},
+      {}
     );
 
     // Grab model's holdings and its details
     const [modelHoldingsResponse, allocationResponse] = await Promise.all([
       fetchModelHolding(Object.keys(allocCompoObj)),
-      fetchAllocation(allocationId),
+      fetchAllocation(allocationId)
     ]);
 
     // Grab all security's id and put in an array
@@ -186,26 +176,26 @@ export async function getRecommendedPortfolio(event, context, callback) {
         const {
           security_id: securityId,
           strategic_weight: weight,
-          model_id: modelId,
+          model_id: modelId
         } = curr;
         const assetWeight = Math.round(
-          (allocCompoObj[modelId].weight / 100) * weight,
+          (allocCompoObj[modelId].weight / 100) * weight
         );
         acc[securityId] = { weight: assetWeight };
         return acc;
       },
-      {},
+      {}
     );
 
     // Get details of the securities
     const securityRequestConfig = {
       query: {
-        filter: `id==${Object.keys(securitiesArr).join(',id==')}`,
-      },
+        filter: `id==${Object.keys(securitiesArr).join(",id==")}`
+      }
     };
     const securitiesRes = await httpClient(ACCESS_TOKEN)(
-      '/security',
-      securityRequestConfig,
+      "/security",
+      securityRequestConfig
     );
 
     // Add more details
@@ -214,8 +204,8 @@ export async function getRecommendedPortfolio(event, context, callback) {
       ...{
         name: security.name,
         ticker: security.ticker,
-        assetClass: security.asset_class,
-      },
+        assetClass: security.asset_class
+      }
     }));
 
     // when traslate /allocation_composition's weight
@@ -223,22 +213,22 @@ export async function getRecommendedPortfolio(event, context, callback) {
     roundValuesUpToTarget({
       source: holdingsArr,
       target: 100,
-      accessorKey: 'weight',
+      accessorKey: "weight"
     });
 
     // Sort by holding's weight
-    const results = sortArrayByDesc(holdingsArr, 'weight');
+    const results = sortArrayByDesc(holdingsArr, "weight");
 
     const response = {
       statusCode: 200,
       headers: {
-        'Access-Control-Allow-Origin': 'http://localhost:3000', // Need to properly set origin to receive response!
-        'Access-Control-Allow-Credentials': true, // Required for cookies, authorization headers with HTTPS
+        "Access-Control-Allow-Origin": "http://localhost:3000", // Need to properly set origin to receive response!
+        "Access-Control-Allow-Credentials": true // Required for cookies, authorization headers with HTTPS
       },
       body: JSON.stringify({
         holdings: results,
-        portfolio: allocationResponse.body,
-      }),
+        portfolio: allocationResponse.body
+      })
     };
 
     callback(null, response);
@@ -246,12 +236,12 @@ export async function getRecommendedPortfolio(event, context, callback) {
     const response = {
       statusCode: error.statusCode,
       headers: {
-        'Access-Control-Allow-Origin': '*', // Required for CORS support to work
-        'Access-Control-Allow-Credentials': true, // Required for cookies, authorization headers with HTTPS
+        "Access-Control-Allow-Origin": "*", // Required for CORS support to work
+        "Access-Control-Allow-Credentials": true // Required for cookies, authorization headers with HTTPS
       },
       body: JSON.stringify({
-        message: error.statusMessage,
-      }),
+        message: error.statusMessage
+      })
     };
 
     callback(null, response);
